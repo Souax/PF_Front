a0eec5f8faa9d78db8e4d080f6efa73a15ad15d5
宮下颯太 <sou@MacBook-Air-M2.local>
2024-03-28 16:41:25 +0900

    toppage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import BookRecommendation from "./components/BookHome/BookRecommendation";
+import Link from "next/link";
+import Book from "./components/BookHome/PopularityBookHome";
+import {
+  getNewBooks,
+  getPopularityBooks,
+  getRecommendationBooks,
+} from "@/src/TopPageAPI";
+import TopPage from "./popularity/TopSlide";
+import PopularityBook from "./components/BookHome/PopularityBookHome";
+import PopularityPage from "./popularity/PopularityPage";
 
-function MainPage() {
-  return <div>page</div>;
+export default async function Home() {
+  const newbooks = await getNewBooks();
+  const popularitybooks = await getPopularityBooks();
+  const recommendationbooks = await getRecommendationBooks();
+
+  return (
+    <>
+      <main>
+        <TopPage newbooks={newbooks} />
+        <PopularityBook popularitybooks={popularitybooks} />
+        <BookRecommendation recommendationbooks={recommendationbooks} />
+        <div className="flex justify-center items-center pb-10">
+          <div className="flex mt-[3rem] bg-[#5A5A5A] rounded-full w-[40rem] py-[0.8rem] text-white text-center items-center justify-center cursor-pointer hover:bg-[#3A3A3A]">
+            <Link href="/AllBooks" className="relative flex">
+              全ての技術書を見る
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
-
-export default MainPage;
