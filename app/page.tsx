7a6b59017a4aea02713f73740058e6e64a2cb171
宮下颯太 <sou@MacBook-Air-M2.local>
2024-03-28 16:45:23 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -14,15 +14,14 @@ import PopularityPage from "./popularity/PopularityPage";
 
 export default async function Home() {
   const newbooks = await getNewBooks();
-  const popularitybooks = await getPopularityBooks();
-  const recommendationbooks = await getRecommendationBooks();
+  //const popularitybooks = await getPopularityBooks();
+  //const recommendationbooks = await getRecommendationBooks();
 
   return (
     <>
       <main>
         <TopPage newbooks={newbooks} />
-        <PopularityBook popularitybooks={popularitybooks} />
-        <BookRecommendation recommendationbooks={recommendationbooks} />
+
         <div className="flex justify-center items-center pb-10">
           <div className="flex mt-[3rem] bg-[#5A5A5A] rounded-full w-[40rem] py-[0.8rem] text-white text-center items-center justify-center cursor-pointer hover:bg-[#3A3A3A]">
             <Link href="/AllBooks" className="relative flex">
